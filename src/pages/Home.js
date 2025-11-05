b6a2b37d1842fdc06721c1567d1702d03f13c323
Piyush Tamoli <piyushmodi812@gmail.com>
2025-11-05 21:45:30 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import HeroSection from '../components/HeroSection';
+
 import Hero from '../components/hero';
 
 
 function Home() {
   return (
     <main>
-        <HeroSection />
         <Hero />
-        <HeroSection />
+  
     </main>
   );
 }
+
 
 export default Home;
